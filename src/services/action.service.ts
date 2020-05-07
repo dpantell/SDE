71088314d8607dc0a9904baa0e5204651e1e683a
Vinnie <vinnie.rossi12@gmail.com>
2020-05-06 21:28:10 -0400

    Update action.service.ts

--- src/services/action.service.ts
+++ src/services/action.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { PhaseAction } from 'src/models/phase.interface';
+import { each } from 'lodash';
 
 @Injectable({ providedIn: 'root' })
 export class ActionService {
 
     public executePhaseActions(actions: PhaseAction[]) {
 
+        each(actions, action => this.executeAction(action));
     }
 
     public executeQueuedActions(actions: any[]) { // RoleAction
 
+        each(actions, action => this.executeAction(action));
     }
 
-    private executeAction(action: PhaseAction): boolean {
+    private executeAction(action: PhaseAction) {
 
-        return true;
     }
 }
